92b56fa409f96ab10a8efa4cce1f3f3074d2353d
Gustavo Garofalo <gustavogarofalo@Gustavos-MBP.cfl.rr.com>
2019-09-15 14:20:27 -0400

    python homework submission

---
+++ solved/PyBank/main.py
@@ -0,0 +1,77 @@
+#insert the necessary modules
+import os
+import csv
+import pandas as pd
+
+#variable storage for month count and profit/loss sum
+profloss_sum=0
+month_count=0
+
+#create variable path
+csv_path=os.path.join('..','..','Instructions', 'PyBank', 'Resources', 'budget_data.csv')
+
+
+#open and read through the file, using the path created
+#creating a variable to store the csv file, each line as a list
+with open(csv_path, newline='') as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=',')
+    
+
+#takind out the column labels, separating each label in one row
+    csv_labels=next(csv_reader)
+     #print(f'csv_header{csv_labels}')
+    date_lab=csv_labels[0]
+    profloss_lab=csv_labels[1]
+
+#loop through the each column using list comprehension
+    for row in csv_reader:
+        if row[0] is not None:
+            month_count += 1
+        if row is not None:
+            profloss_sum += int(row[1])
+
+#creating panda dataframe with the module    
+df_file=pd.read_csv(csv_path)
+#applying difference function to create a new Delta column
+df_file['Delta']=df_file['Profit/Losses'].diff().shift(0)
+delta_avr=df_file['Delta'].mean()
+low_diff=df_file['Delta'].min()
+high_diff=df_file['Delta'].max()
+
+#Setting Delta column as index to look for the respective date of maximum decrease
+df_new=df_file.set_index('Delta')
+min_date=df_new.loc[low_diff, 'Date']
+max_date=df_new.loc[high_diff, 'Date']
+
+
+#printing all the solutions
+# 
+print('                               ') 
+print("Financial Analysis")
+print('-------------------------------')
+print('Total Months: ', month_count)
+print('Total: $', profloss_sum)
+print("Average Change: ", round(delta_avr,2))
+print('Greatest Increase in Profits: ', max_date, '($', int(high_diff), ')' )
+print('Greatest Decrease in Profits: ', min_date, '($', int(low_diff), ')')
+    
+
+#writing the results in a new txt file
+
+output_path=os.path.join('output.csv')
+
+#assessing output file but for write mode now
+with open(output_path, 'w', newline='') as output_file:
+    output_writer=csv.writer(output_file)
+
+    #create the first line for the headers
+    output_writer.writerow(['                               '])
+    output_writer.writerow(["Financial Analysis"])
+    output_writer.writerow(['-------------------------------'])
+    output_writer.writerow(['Total Months: ', month_count])
+    output_writer.writerow(['Total: $', profloss_sum])
+    output_writer.writerow(["Average Change: ", round(delta_avr,2)])
+    output_writer.writerow(['Greatest Increase in Profits: ', max_date, '($', int(high_diff), ')' ])
+    output_writer.writerow(['Greatest Decrease in Profits: ', min_date, '($', int(low_diff), ')'])
+   
+    
---
+++ solved/PyPoll/main.py
@@ -0,0 +1,96 @@
+#importing necessary modules
+import os
+import csv
+
+#count variables
+vote_count=0
+khan_count=0
+correy_count=0
+li_count=0
+tooley_count=0
+
+
+#Empty lists
+winner=[]
+
+#setting up the path for file into a variable
+csv_path=os.path.join('..', '..', 'Instructions', 'PyPoll', 'Resources', 'election_data.csv')
+
+#Opening and reading file, storing in pypoll variable
+with open(csv_path, newline='') as csv_file:
+    pypoll=csv.reader(csv_file, delimiter=',')
+
+#Taking out headers of the database
+    pypoll_labels=next(pypoll)
+
+#Loop trough voter id column to count and to store percentsge of votes
+    
+    for row in pypoll:
+        if row[0] is not None:
+            vote_count += 1
+
+        if row[2] == "Khan":
+            khan_count += 1
+            k_percent=round((khan_count/vote_count)*100)
+
+        if row[2] == "Correy":
+            correy_count += 1
+            c_percent= round((correy_count/vote_count)*100)
+
+        if row[2] == "Li":
+            li_count += 1
+            l_percent=round((li_count/vote_count)*100)
+
+        if row[2] == "O'Tooley":
+            tooley_count +=1
+            t_percent=round((tooley_count/vote_count)*100)
+
+#Conditionals statement to exhibit the winner by comparing the percentages of votes
+
+    if (c_percent<k_percent) and (l_percent < k_percent) and (t_percent < k_percent):
+        w="Khan"
+        winner.append(w)
+    
+    if (c_percent<t_percent) and (l_percent < t_percent) and (k_percent < t_percent):
+        w="O`Tooley"
+        winner.append(w)
+    
+    if (c_percent<l_percent) and (k_percent < l_percent) and (t_percent < l_percent):
+        w="Li"
+        winner.append(w)
+
+    if (k_percent<c_percent) and (l_percent < c_percent) and (t_percent < c_percent):
+        w="Correy"
+        winner.append(w)
+    
+
+#Printing all of the results
+print('Election Results')
+print('-----------------------------------')
+print('Total Votes: ', vote_count)
+print('-----------------------------------')
+print("Khan: ", k_percent, '%', '(', khan_count, ')')
+print("Correy: ", c_percent, '%', '(', correy_count, ')')
+print("Li: ", l_percent, '%', '(', li_count, ')')
+print("O'Tooley: ", t_percent, '%', '(', tooley_count, ')')
+print('-----------------------------------')
+print("Winner :", winner)
+print('-----------------------------------')
+
+
+output_path='output.csv'
+with open(output_path, 'w', newline='') as output_file:
+    output_writer=csv.writer(output_file)
+
+    #create the first line for the headers
+    output_writer.writerow(['Election Results'])
+    output_writer.writerow(['-----------------------------------'])
+    output_writer.writerow(['Total Votes: ', vote_count])
+    output_writer.writerow(['-----------------------------------'])
+    output_writer.writerow(["Khan: ", k_percent, '%', '(', khan_count, ')'])
+    output_writer.writerow(["Correy: ", c_percent, '%', '(', correy_count, ')'])
+    output_writer.writerow(["Li: ", l_percent, '%', '(', li_count, ')' ])
+    output_writer.writerow(["O'Tooley: ", t_percent, '%', '(', tooley_count, ')'])
+    output_writer.writerow(['-----------------------------------'])
+    output_writer.writerow(["Winner :", winner])
+    output_writer.writerow(['-----------------------------------'])
